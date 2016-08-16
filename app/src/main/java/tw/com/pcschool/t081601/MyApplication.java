713e09962a6ec34eef47f2840a9a393bc91b7cd5
shaojung <shaojung@gmail.com>
2016-08-16 15:40:40 +0800

    Initial commit

---
+++ app/src/main/java/tw/com/pcschool/t081601/MyApplication.java
@@ -0,0 +1,11 @@
+package tw.com.pcschool.t081601;
+
+import android.app.Application;
+
+/**
+ * Created by Student on 2016/8/16.
+ */
+public class MyApplication
+        extends Application{
+    int test;
+}
